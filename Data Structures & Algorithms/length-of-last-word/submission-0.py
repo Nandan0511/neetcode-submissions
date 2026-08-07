9481657b9079cc3e7e0f85beef349960eaf2861a
Nandan0511 <nandan000070@gmail.com>
2026-08-07 13:56:10 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        ans = 0
+        s = s.strip()
+
+        for i in range(len(s)-1,-1,-1):
+
+            if s[i] == " ":
+                break
+            ans +=1
+        
+        return ans
